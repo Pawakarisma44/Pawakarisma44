b110ae4bb5e5ba28bdde0ee3f3a9a5bda71713e6
Pawakarisma44 <114038147+Pawakarisma44@users.noreply.github.com>
2022-09-21 08:14:33 +0700

    Add files via upload

---
+++ threadlatihan/src/threadlatihan/Threadlatihan.java
@@ -0,0 +1,29 @@
+
+package threadlatihan;
+import static java.lang.Thread.sleep;
+
+
+public class Threadlatihan {
+    
+    public static void main(String[] args) {
+        Thread t1=new Proses1();
+ Thread t2=new Proses2();
+ 
+ t1.start();
+ t2.start();
+ } 
+}
+class Proses1 extends Thread{
+ public void run(){
+ try{
+ sleep(300);
+ }catch (InterruptedException e){
+ e.printStackTrace();}
+ System.out.println("Proses 1 Berjalan");
+ }
+}
+class Proses2 extends Thread{
+ public void run(){
+ System.out.println("Proses 2 Berjalan");
+ }
+}
---
+++ threadlatihan/src/threadlatihan/threadtugas.java
@@ -0,0 +1,35 @@
+
+package threadlatihan;
+
+public class threadtugas {
+
+    public static void main(String[] args) {
+        Thread celana1 = new Thread(new Celana("Celana-1"));
+        Thread celana2 = new Thread(new Celana("Celana-2"));
+        celana1.start();
+        celana2.start();
+    }
+}
+
+class Celana implements Runnable {
+
+    String nama;
+// konstruktor
+    public Celana(String id) {
+        nama = id;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException ie) {
+                System.out.println("Terinterupsi");
+            }
+            System.out.println("Thread " + nama + ":Posisi" + i);
+        }
+    }}
+        
+    
+    
+
